2700828adc49e01d3e244f28d4a6b81ce9ae6a89
stephenie11 <pirvu.stefaniaemilia@gmail.com>
2017-05-05 01:01:53 +0300

    Hello world!

--- src/kulami/Kulami.java
+++ src/kulami/Kulami.java
@@ -15,7 +15,7 @@ public class Kulami {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello fellows!");
     }
     
 }
