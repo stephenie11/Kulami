a8cfd1579af3931011b620da6824817c0f69dbc2
mirunastefania <miruna.oancea.mo@gmail.com>
2017-05-21 21:12:45 +0300

    Add files via upload

---
+++ objects/PlayerNames.java
@@ -0,0 +1,19 @@
+package objects;
+
+import java.io.Serializable;
+
+public class PlayerNames implements Serializable{
+	
+	String Name;
+
+	public String getName() {
+		return Name;
+	}
+
+	public PlayerNames(String name) {
+		super();
+		Name = name;
+	}
+	
+
+}
